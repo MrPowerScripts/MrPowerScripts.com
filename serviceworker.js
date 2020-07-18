3a291732b144da827900a87a8d10b62e7f89b801
MrPowerCI <playingwithpowershell@gmail.com>
2020-07-18 13:39:56 +0000

    [skip ci] Deploy updates

--- serviceworker.js
+++ serviceworker.js
@@ -12,40 +12,40 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
-  var cacheKeeplist = ['serviceCache'];
+// self.addEventListener('activate', function(event) {
+//   var cacheKeeplist = ['serviceCache'];
 
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
+//   event.waitUntil(
+//     caches.keys().then((keyList) => {
+//       return Promise.all(keyList.map((key) => {
+//         if (cacheKeeplist.indexOf(key) === -1) {
+//           return caches.delete(key);
+//         }
+//       }));
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  if (event.request.method !== 'GET') { return }
+// self.addEventListener('fetch', function(event) {
+//   if (event.request.method !== 'GET') { return }
 
-  if (
-    event.request.url.includes('amazon-adsystem') ||
-    event.request.url.includes('browser-sync') ||
-    event.request.url.includes('media-amazon') ||
-    event.request.url.includes('cloudflare-static') ||
-    event.request.url.includes('google-analytics') ) {
-    return false;
-  }
+//   if (
+//     event.request.url.includes('amazon-adsystem') ||
+//     event.request.url.includes('browser-sync') ||
+//     event.request.url.includes('media-amazon') ||
+//     event.request.url.includes('cloudflare-static') ||
+//     event.request.url.includes('google-analytics') ) {
+//     return false;
+//   }
 
-  event.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+//   event.respondWith(
+//     caches.open(cacheName).then(function(cache) {
+//       return cache.match(event.request).then(function (response) {
+//         return response || fetch(event.request).then(function(response) {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
